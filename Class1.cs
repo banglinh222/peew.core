6b2f342ac6ad1ab6c8df6b89b84990ef3f12419b
bllovesgnr <bllovesgnr@gmail.com>
2021-04-18 21:48:29 +0900

    init

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace peew
+{
+    public class PeewClass
+    {
+        public static void writeConsole(String str)
+        {
+            Console.Write(str);
+        }
+    }
+}
