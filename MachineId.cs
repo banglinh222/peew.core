fec58c217cfd5589581a289141ad9149d19e1827
bllovesgnr <bllovesgnr@gmail.com>
2021-04-20 18:37:16 +0900

    add machine Id get

---
+++ MachineId.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace peew
+{
+    public class MachineId
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public static async Task<string> get()
+        {
+            var response = await client.PostAsync("https://dev.peew.me/machine-id", null).ConfigureAwait(false);
+            string str_json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            dynamic json = JsonConvert.DeserializeObject(str_json);
+            return json.machineId;
+        }
+    }
+}
